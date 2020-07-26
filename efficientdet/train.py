6a9f95517f27de32c62dc3cff2b7a5f6fa38eece
OrKatz7 <Or.Katz1@gmail.com>
2020-07-26 12:48:19 +0300

    Update train.py

--- efficientdet/train.py
+++ efficientdet/train.py
@@ -39,52 +39,81 @@ class Config:
     lf = lambda x: (((1 + math.cos(x * math.pi / 25)) / 2) ** 1.0) * 0.9 + 0.1
     scheduler = torch.optim.lr_scheduler.LambdaLR
 
-
+class data_config:
+    real = 0.5
+    mosaic = 0.3
+    cutmix = 0.1
+    stylized = 0.2
+    
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--fold', type=int, default=0)
-    parser.add_argument('--resume', action='store_true', help='resume training from last.pt',default=None)
+    parser.add_argument('--resume', type=str, help='resume training from last.pt',default=None)
     opt = parser.parse_args()
     config = Config()
     config.fold_number = opt.fold
     config.resume = opt.resume
-    folder = '../effdet7_fold{}'.format(config.fold_number)
+    folder = 'effdet7_fold{}'.format(config.fold_number)
     if not os.path.exists(folder):
         os.makedirs(folder)
     config.folder = folder
-    marking,df_folds = train_utils.get_k_fols(config)
     
-    train_dataset = datasets_utils.train_wheat(image_ids=df_folds[df_folds['fold'] != config.fold_number].index.values,
-                                marking=marking,
-                                transforms=datasets_utils.get_train_transforms(),
-                                test=False,
-                                TRAIN_ROOT_PATH=config.root_data)
+    if not config.CocoFormat:
+        marking,df_folds,spike = train_utils.get_k_fols(config)
 
-    validation_dataset = datasets_utils.DatasetRetrieverTest(image_ids=df_folds[df_folds['fold'] == config.fold_number].index.values,
-                                              marking=marking,
-                                              transforms=datasets_utils.get_valid_transforms(),
-                                              test=True,
-                                              TRAIN_ROOT_PATH=config.root_data)
-    
+
+        train_dataset = datasets_utils.train_wheat(image_ids=df_folds[df_folds['fold'] != config.fold_number].index.values,
+                                    marking=marking,
+                                    data_config = data_config,
+                                    transforms=datasets_utils.get_train_transforms(),
+                                    test=False,
+                                    TRAIN_ROOT_PATH=config.root_data)
+
+        validation_dataset = datasets_utils.DatasetRetrieverTest(image_ids=df_folds[df_folds['fold'] == config.fold_number].index.values,
+                                                  marking=marking,
+                                                  transforms=datasets_utils.get_valid_transforms(),
+                                                  test=True,
+                                                  TRAIN_ROOT_PATH=config.root_data)
+    else:
+        train_csv = pd.read_csv("cocoFormat/tile_train.csv")
+        pseudo_csv = pd.read_csv("cocoFormat/pseudo_train.csv")
+        val_csv = pd.read_csv("cocoFormat/tile_val.csv")
+        train_csv = pd.concat([train_csv,pseudo_csv])
+
+        train_dataset = datasets_utils.train_wheat(image_ids=np.array(list(set(train_csv.image_id))),
+                                    marking=train_csv,
+                                    data_config=data_config,
+                                    transforms=datasets_utils.get_train_transforms(),
+                                    test=False,
+                                    TRAIN_ROOT_PATH=config.root_data)
+
+        validation_dataset = datasets_utils.DatasetRetrieverTest(image_ids=np.array(list(set(val_csv.image_id))),
+                                                  marking=val_csv,
+                                                  transforms=datasets_utils.get_valid_transforms(),
+                                                  test=True,
+                                                  TRAIN_ROOT_PATH=config.root_data)
+
     train_loader = torch.utils.data.DataLoader(
-                                                train_dataset,
-                                                batch_size=config.batch_size,
-                                                sampler=RandomSampler(train_dataset),
-                                                pin_memory=False,
-                                                drop_last=True,
-                                                num_workers=config.num_workers,
-                                                collate_fn=datasets_utils.collate_fn
-                                               )
+                                                    train_dataset,
+                                                    batch_size=config.batch_size,
+                                                    sampler=RandomSampler(train_dataset),
+                                                    pin_memory=False,
+                                                    drop_last=True,
+                                                    num_workers=config.num_workers,
+                                                    collate_fn=datasets_utils.collate_fn
+                                                   )
     validation_loader = torch.utils.data.DataLoader(
-                                                validation_dataset,
-                                                batch_size=config.batch_size*4,
-                                                shuffle=False,
-                                                num_workers=2,
-                                                drop_last=False,
-                                                collate_fn=datasets_utils.collate_fn
-                                                )
-    
-    net = train_utils.get_net(type_net=config.net_name,checkpoint_name=config.checkpoint_name,resume = config.resume)
+                                                    validation_dataset,
+                                                    batch_size=config.batch_size*4,
+                                                    shuffle=False,
+                                                    num_workers=2,
+                                                    drop_last=False,
+                                                    collate_fn=datasets_utils.collate_fn
+                                                    )
+    if len(config.TrainMultiScale)==1:
+        net = train_utils.get_net(type_net=config.net_name,checkpoint_name=config.checkpoint_name,resume = config.resume)
+    else:
+        net = train_utils.get_net_multiscle(type_net=config.net_name,checkpoint_name=config.checkpoint_name,resume = config.resume,multiScale=config.TrainMultiScale)
     net.cuda()
     fitter = train_utils.Fitter(model=net, config=config)
     fitter.fit(train_loader, validation_loader)
