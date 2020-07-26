c5c03cc1400162a04a644dfd3336759dd02eca7f
OrKatz7 <Or.Katz1@gmail.com>
2020-07-26 12:47:30 +0300

    Update __init__.py

--- efficientdet/effdet/__init__.py
+++ efficientdet/effdet/__init__.py
@@ -1,4 +1,4 @@
 from .efficientdet import EfficientDet
-from .bench import DetBenchEval, DetBenchTrain
+from .bench import DetBenchEval, DetBenchTrain,DetBenchTrainMultiScale
 from .config.config import get_efficientdet_config
 from .helpers import load_checkpoint, load_pretrained
